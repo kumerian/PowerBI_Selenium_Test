b064fa0a6e8317f7f99370804a963005241e32d5
Koen van der Pasch <croga@croga.com>
2020-08-08 08:28:32 +0200

    Initiate new Maven project

---
+++ src/main/java/com/croga/pbiseltest/PowerBiSeleniumTestApplication.java
@@ -0,0 +1,13 @@
+package com.croga.pbiseltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerBiSeleniumTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerBiSeleniumTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/croga/pbiseltest/PowerBiSeleniumTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.croga.pbiseltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerBiSeleniumTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
